49770624fb1e2785a16195e48d589567b3f3dece
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-07-31 11:25:31 +0200

    Autoplay

--- watch.lua
+++ watch.lua
@@ -1,6 +1,10 @@
-local id = window.query.id
+local id = location.query.id
 
 local res = fetch({
   url = "https://api.fsh.plus/video?id=" .. id
-});
-get('video').set_source(res.video)
+}):await();
+get_id('video').content = res.video
+
+if browser.api.media_context then
+  get_id('video').media_context.play()
+end
